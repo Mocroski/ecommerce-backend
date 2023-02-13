3c3ec40bd67ed160b67523e03193ee3bea251ed9
LukeMocroski <devmocroski@gmail.com>
2023-02-13 15:54:29 -0300

    CRUD Estado

---
+++ src/main/java/com/devmocroski/backend/repository/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.devmocroski.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devmocroski.backend.entity.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long>{
+
+	
+}
