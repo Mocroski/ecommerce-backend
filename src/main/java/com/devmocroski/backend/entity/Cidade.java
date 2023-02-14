fda225d37e55a5e16a685f2aec106d0be6594b7c
LukeMocroski <devmocroski@gmail.com>
2023-02-14 09:10:42 -0300

    Att project

---
+++ src/main/java/com/devmocroski/backend/entity/Cidade.java
@@ -0,0 +1,36 @@
+package com.devmocroski.backend.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Table(name = "cidade")
+@Data
+public class Cidade {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCriacao;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataAtualizacao;
+	
+	@ManyToOne
+	@JoinColumn("idEstado")
+	private Estado estado;
+}
